85461545eb682c10b1b4ac8c67fb5d8aadbf5d2f
ftgroup <firman.efendi@gmail.com>
2012-08-16 12:36:14 +0700

    v0.0.0.7 observer to add button block

---
+++ app/code/community/Tebet/SocialShare/Model/Observer.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Observer
+ * 
+ * @category    Social Share Buttons
+ * @package     Tebet_SocialShare
+ * @copyright   Copyright (c) 2012 Firman Efendi (firman.efendi@gmail.com)
+ * @website		http://www.theyoungcandidate.com/
+ */
+class Tebet_SocialShare_Model_Observer
+{
+	public function addButtonBlock($observer)
+    {
+		$block = $observer->getEvent()->getBlock();
+		
+		if (($block->getAction()->getFullActionName() == 'catalog_product_view') && ($block->getNameInLayout() == 'product.info')) {
+			
+			$layout	= $block->getLayout();
+			$name 	= (string) Mage::helper('socialshare')->getBlockRef();
+			$alias 	= $name;
+			
+			$block->setChild($alias,
+						$layout->createBlock('socialshare/button',
+							$name,
+							array('template'=>'tebet/socialshare/button.phtml')
+						)
+					);
+		}
+	}
+}
